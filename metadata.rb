a33416027018f1de015ea22154cb0d3a02bb7dd9
Thijs Houtenbos <thoutenbos@schubergphilis.com>
2017-04-05 13:02:04 +0200

    respond_to

--- metadata.rb
+++ metadata.rb
@@ -14,6 +14,7 @@ supports 'centos'
 # for secrets management in setup_auth recipe
 depends 'chef-vault', '>= 1.0.4'
 
-source_url 'https://github.com/chef-cookbooks/chef-splunk'
-issues_url 'https://github.com/chef-cookbooks/chef-splunk/issues'
+source_url 'https://github.com/chef-cookbooks/chef-splunk' if respond_to?(:source_url)
+issues_url 'https://github.com/chef-cookbooks/chef-splunk/issues' if respond_to?(:issues_url)
+
 chef_version '>= 12.1' if respond_to?(:chef_version)
